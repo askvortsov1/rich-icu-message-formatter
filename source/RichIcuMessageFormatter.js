5b07fba4d4aa75a106493c3a5a4b25f237095ab1
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-01-16 23:05:49 -0500

    Export unEscapeHtml

--- source/RichIcuMessageFormatter.js
+++ source/RichIcuMessageFormatter.js
@@ -1,3 +1,4 @@
 export { default as RichMessageFormatter } from './RichMessageFormatter.js';
 export { default as defaultRichHandler } from './defaultRichHandler';
 export { default as mithrilRichHandler } from './mithrilRichHandler';
+export { unEscapeHtml } from './utilities';
